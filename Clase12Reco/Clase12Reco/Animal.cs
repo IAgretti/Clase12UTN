5bd26a90ef93cd1018b33adc5adfae28a403d8ce
Iri <89229099+IAgretti@users.noreply.github.com>
2023-09-12 11:17:48 -0300

    Ejercicio ejemplo clase 12

---
+++ Clase12Reco/Clase12Reco/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase12Reco
+{
+    internal class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Ha nacido un animal!!!");
+        }
+    }
+    class Perro: Animal
+    {
+        public Perro(string n)
+        {
+            Console.WriteLine("\nHa nacido un Perro!!\nSe llama: " + n + "\n");
+        }
+    }
+    class Gato: Animal
+    {
+        public Gato()
+        {
+            Console.WriteLine("\nHa nacido un Gato!!!");
+        }
+        public Gato(string n)
+        {
+            Console.WriteLine("\nHa nacido un Gato!!\nSe llama: " + n + "\n");
+        }
+    }
+    class GatoSiames: Gato
+    {
+        public GatoSiames(string n)
+        {
+            Console.WriteLine("\nHa nacido un gato Siamés!!\nSe llama: " + n + "\n");
+        }
+    }
+}
---
+++ Clase12Reco/Clase12Reco/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Clase12Reco
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Animal a = new();
+            //GatoSiames gs = new GatoSiames("Pinino");
+            GatoSiames gs = new("Pinino");
+            Perro p= new Perro("Pancho");
+            Gato g = new Gato("Michifus");
+            Console.ReadKey();
+        }
+    }
+}
